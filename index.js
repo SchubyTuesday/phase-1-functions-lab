5450d0e5d56f2f607cd2a78374301765f51ca5cd
Charlie Schubach <charlie.schubach@gmail.com>
2023-12-11 12:10:13 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(42 - someValue)
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue)*264
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start)*264
+  }
+
+  function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    let totalFare = ""
+    //First 400: free
+    if (distance <= 400) {
+        return 0
+    } else if (distance <= 2000) {
+        return (distance - 400)*0.02
+    } else if (distance <= 2500) {
+        return 25
+    } {return "cannot travel that far"} 
+
+    return totalFare
+    //next 1600: .02/ft
+    //2000-2500
+  }
